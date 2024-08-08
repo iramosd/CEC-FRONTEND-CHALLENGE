58877412662666d7d605d1abc72149edefbacd65
iramosd <ramosdumas_ismael@hotmail.com>
2024-08-08 17:00:38 -0600

    Detail product page finished. Add rating component. Update layout and components. Add social icons.

--- src/components/DetailProduct.vue
+++ src/components/DetailProduct.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { useProducts } from "../composables/useProducts.ts";
+import Rating from "./Rating.vue";
 
 const props = defineProps({
   id:{
@@ -9,10 +11,67 @@ const props = defineProps({
 });
 
 const { error, product, getProduct } = useProducts();
+const count = ref(0);
 await getProduct(props.id);
 
 </script>
 
 <template>
-  <p>{{ product }}</p>
+  <header>
+    <div class="bg-[#F9F1E7] text-[#9F9F9F] w-full p-8 lg:py-10 lg:px-20 flex gap-2 lg:gap-6">
+      <a class="flex items-center" href="/">Home</a>
+      <img src="/icons/arrow.svg" alt="arrow">
+      <a class="flex items-center" href="/">{{ product.category }}</a>
+      <img src="/icons/arrow.svg" alt="arrow">
+      <span class="w-[2px] bg-[#9F9F9F]"></span>
+      <span class="text-black">{{ product.title }}</span>
+    </div>
+  </header>
+
+  <div class="p-8 lg:px-20 lg:py-10 flex flex-col lg:flex-row gap-10 sm:gap-20">
+    <div class="flex flex-col sm:flex-row w-full lg:w-1/2 gap-4 sm:gap-10">
+      <div class="grid grid-cols-4 sm:flex sm:flex-col gap-2 sm:gap-6 w-full sm:w-2/12">
+        <img class="object-contain sm:h-24 sm:w-24 rounded-lg sm:rounded-xl border-solid border-2" :src="product.image" alt="`${product.title}`">
+      </div>
+      <img class="order-first sm:order-last sm:w-9/12 object-contain rounded-lg sm:rounded-xl sm:h-[70vh] border-solid border-2" :src="product.image" :alt="`${product.title}`">
+    </div>
+    <div class="w-full lg:w-1/2 flex flex-col gap-5">
+      <h3 class="text-[42px]">{{ product.title }}</h3>
+      <p class="text-[#9F9F9F] text-2xl">U$ {{ product.price }}</p>
+      <div class="flex gap-3">
+        <Rating :rate="product.rating.rate"/>
+        <span class="w-[1px] bg-[#9F9F9F]"></span>
+        <span class="text-sm text-[#9F9F9F]">{{ product.rating.count }} Customer {{ product.rating.count > 1 ? 'Reviews' : 'Review'}}</span>
+      </div>
+      <p class="w-10/12">{{ product.description }}</p>
+      <div class="flex flex-col gap-6">
+        <div class="flex flex-row w-full gap-4">
+          <div class="rounded-lg flex justify-between items-center p-3 border border-[#9F9F9F]">
+            <button @click="count--" class="hover:bg-gray-300 duration-300 rounded-full w-5 h-5 flex items-center justify-center">-</button>
+            <span>{{ count }}</span>
+            <button  @click="count++" class="hover:bg-gray-300 duration-300 rounded-full w-5 h-5 flex items-center justify-center">+</button>
+          </div>
+          <button class="rounded-lg disabled:opacity-50 disabled:bg-black disabled:text-white text-xl p-3 border border-black hover:bg-black hover:text-white duration-300">Add To Cart</button>
+        </div>
+      </div>
+      <span class="h-[1px] bg-[#D9D9D9] my-8"></span>
+      <div class="text-[#9F9F9F] flex flex-col gap-4">
+          <div class="flex gap-3">
+            <span class="w-20">SKU</span>
+            <span>:</span><span>SS00{{product.id}}</span>
+          </div>
+        <div class="flex gap-3">
+          <span class="w-20">Category</span><span>:</span><span>{{ product.category }}</span></div>
+        <div class="flex gap-3">
+          <span class="w-20">Share</span><span>:</span>
+          <div class="text-black flex gap-4 text-2xl">
+            <a href="#"><img src="/icons/facebook.svg" alt="facebook"></a>
+            <a href="#"><img src="/icons/linkedin.svg" alt="linkedin"></a>
+            <a href="#"><img src="/icons/twitter.svg" alt="twitter"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </template>
--- src/components/Features.vue
+++ src/components/Features.vue
@@ -4,28 +4,28 @@
 <template>
   <div class="bg-[#FAF3EA] gap-10 lg:gap-2 p-8 lg:px-12 lg:py-20 flex flex-wrap justify-between">
     <div class="flex items-center gap-3">
-      <img loading="lazy" class="w-10 lg:w-fit" src="/icons/trophy.svg" alt="trophy">
+      <img class="w-10 lg:w-fit" src="/icons/trophy.svg" alt="trophy">
       <div>
         <p class="font-semibold text-xl lg:text-[25px] text-[#242424]">High Quality</p>
         <p class="font-medium text-[#898989] lg:text-[20px]">crafted from top materials</p>
       </div>
     </div>
     <div class="flex items-center gap-3">
-      <img loading="lazy" class="w-10 lg:w-fit" src="/icons/guarantee.svg" alt="guarantee">
+      <img class="w-10 lg:w-fit" src="/icons/guarantee.svg" alt="guarantee">
       <div>
         <p class="font-semibold text-xl lg:text-[25px] text-[#242424]">Warranty Protection</p>
         <p class="font-medium text-[#898989] lg:text-[20px]">Over 2 years</p>
       </div>
     </div>
     <div class="flex items-center gap-3">
-      <img loading="lazy" class="w-10 lg:w-fit" src="/icons/shipping.svg" alt="shipping">
+      <img class="w-10 lg:w-fit" src="/icons/shipping.svg" alt="shipping">
       <div>
         <p class="font-semibold text-xl lg:text-[25px] text-[#242424]">Free Shipping</p>
         <p class="font-medium text-[#898989] lg:text-[20px]">Order over 150 $</p>
       </div>
     </div>
     <div class="flex items-center gap-3">
-      <img loading="lazy" class="w-10 lg:w-fit" src="/icons/customer-support.svg" alt="support">
+      <img class="w-10 lg:w-fit" src="/icons/customer-support.svg" alt="support">
       <div>
         <p class="font-semibold text-xl lg:text-[25px] text-[#242424]">24 / 7 Support</p>
         <p class="font-medium text-[#898989] lg:text-[20px]">Dedicated support</p>
